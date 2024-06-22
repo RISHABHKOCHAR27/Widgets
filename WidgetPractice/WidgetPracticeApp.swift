6fd35a02124ea2920d106c5e3f5ea7c1cdabda12
rishabhkochar27 <rishabhkochar27@gmail.com>
2024-06-22 14:18:38 +0530

    Initial Commit

---
+++ WidgetPractice/WidgetPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetPracticeApp.swift
+//  WidgetPractice
+//
+//  Created by nuclei on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WidgetPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
